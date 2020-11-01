8fa8b146c35c1f4760d3660a57474f1b74104981
Tomson <39458668+t0mson@users.noreply.github.com>
2020-11-01 17:53:01 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,14 @@
 module.exports = {
-   name: 'ping',
-   description: ['Showes the Bot Latency & API Latency.'],
-   async execute(message, args, client) {
-      const msg = await message.channel.send('Pinging...');
-      const latency = msg.createdTimestamp - message.createdTimestamp;
-      msg.edit(
-         `Bot Latency: \`${latency}ms\`, API Latency: \`${Math.round(
-            client.ws.ping
-         )}ms\``
-      );
-   },
+  name: 'ping',
+  aliases: ['p', 'pong'],
+  description: ['Showes the Bot Latency & API Latency.'],
+  async execute(message, args, client) {
+    const msg = await message.channel.send('Pinging...');
+    const latency = msg.createdTimestamp - message.createdTimestamp;
+    msg.edit(
+      `Bot Latency: \`${latency}ms\`, API Latency: \`${Math.round(
+        client.ws.ping
+      )}ms\``
+    );
+  },
 };
